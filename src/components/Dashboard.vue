b1ff2236bb090d9d4539baa8d061ee622f72372e
eggyy1224 <eggyy1224@gmail.com>
2019-12-17 14:54:58 +0800

    Dashboard

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Navbar/>
+    <div class="container-fluid">
+      <div class="row">
+      <Sidebar/>
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+          <router-view></router-view>
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+export default {
+  components: {
+    Navbar,
+    Sidebar,
+  }
+}
+</script>
+
