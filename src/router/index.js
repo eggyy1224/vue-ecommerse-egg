0fa98dc2bdb09ac230744e630a3489aaf2efc010
eggyy1224 <eggyy1224@gmail.com>
2019-12-22 20:44:21 +0800

    coupons and orders router and basic page

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,13 @@ import HelloWorld from '@/components/HelloWorld'
 import Dashboard from '@/components/Dashboard'
 import Login from '@/components/pages/Login'
 import Products from '@/components/pages/Products'
+import Orders from '@/components/pages/Orders'
+import Coupons from '@/components/pages/Coupons'
 
 Vue.use(Router)
 
 export default new Router({
+  linkActiveClass: 'active',
   routes: [
     {
       path: '*',
@@ -27,6 +30,18 @@ export default new Router({
           path: 'products',
           name: 'Products',
           component: Products,
+          meta: { requiresAuth: true },
+        },
+        {
+          path: 'orders',
+          name: 'Orders',
+          component: Orders,
+          meta: { requiresAuth: true },
+        },
+        {
+          path: 'coupons',
+          name: 'Coupons',
+          component: Coupons,
           meta: { requiresAuth: true },
         }
       ]
